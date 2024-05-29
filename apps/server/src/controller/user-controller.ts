2822c77a6a47a7277d7fb0a1d992954537e8202c
dribble-njr <wzw15292257101@163.com>
2024-05-29 23:50:21 +0800

    style: change param name

--- apps/server/src/controller/user-controller.ts
+++ apps/server/src/controller/user-controller.ts
@@ -2,7 +2,7 @@ import { IncomingMessage, ServerResponse } from 'node:http';
 import { BaseController } from './base-controller';
 import { getPostData } from '../util';
 import { UserService } from '../service/user-service';
-import { LoginParmas } from '@imperial-kitchen/types';
+import { SignInParams } from '@imperial-kitchen/types';
 
 export default class UserController extends BaseController {
   private userService: UserService;
@@ -13,7 +13,7 @@ export default class UserController extends BaseController {
   }
 
   async signIn(req: IncomingMessage, res: ServerResponse) {
-    const data = await getPostData<LoginParmas>(req);
+    const data = await getPostData<SignInParams>(req);
     const user = await this.userService.signIn(data);
     UserController.sendResponse(200, user, res);
   }
