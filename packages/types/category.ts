4ba762ce25e66605897fac8aff4ca47e4c1cb4bb
dribble-njr <wzw15292257101@163.com>
2024-07-11 18:33:47 +0800

    feat(types): add category and commondity type

--- packages/types/category.ts
+++ packages/types/category.ts
@@ -1,5 +1,8 @@
+import { Commodity } from './commodity';
+
 export interface Category {
   id: number;
   name: string;
   description?: string;
+  goods: Commodity[];
 }
---
+++ packages/types/commodity.ts
@@ -0,0 +1,6 @@
+export interface Commodity {
+  id: number;
+  name: string;
+  price: number;
+  description?: string;
+}
