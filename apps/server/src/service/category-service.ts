3da54525dfa7148e91b52266b97b6cc0346d1425
dribble-njr <wzw15292257101@163.com>
2024-07-18 17:19:39 +0800

    feat(server): category list

--- apps/server/src/service/category-service.ts
+++ apps/server/src/service/category-service.ts
@@ -7,13 +7,53 @@ export class CategoryService extends BaseService {
   }
 
   async getAllCategories() {
-    const res = await this.db.execute('SELECT * FROM categories');
-    const categories: Category[] = res.rows.map((row) => ({ id: Number(row.id), name: String(row.name) }));
-    return categories;
+    const res = await this.db.execute(`
+      SELECT 
+        c.id AS category_id, 
+        c.name AS category_name, 
+        c.description AS category_description, 
+        g.id AS good_id, 
+        g.name AS good_name, 
+        g.price AS good_price, 
+        g.description AS good_description
+      FROM 
+        category c
+      LEFT JOIN 
+        commodity g 
+      ON 
+        c.id = g.category_id
+    `);
+
+    console.log(res, 'category list');
+
+    const categoryMap = new Map<number, Category>();
+
+    res.rows.forEach((row) => {
+      const categoryId = Number(row.category_id);
+      if (!categoryMap.has(categoryId)) {
+        categoryMap.set(categoryId, {
+          id: categoryId,
+          name: String(row.category_name),
+          description: String(row.category_description),
+          goods: []
+        });
+      }
+
+      if (row.good_id) {
+        categoryMap.get(categoryId)?.goods.push({
+          id: Number(row.good_id),
+          name: String(row.good_name),
+          price: Number(row.good_price),
+          description: row.good_description ? String(row.good_description) : undefined
+        });
+      }
+    });
+
+    return Array.from(categoryMap.values());
   }
 
   async createCategory(name: string) {
-    const res = await this.db.execute({ sql: 'INSERT INTO categories (name) VALUES (?)', args: [name] });
+    const res = await this.db.execute({ sql: 'INSERT INTO category (name) VALUES (?)', args: [name] });
     return res.lastInsertRowid;
   }
 }
