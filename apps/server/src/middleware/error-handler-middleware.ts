eb62e45eb4e4c244cc99d20de723dc544bb86b1a
dribble-njr <wzw15292257101@163.com>
2024-09-29 18:45:35 +0800

    feat(server): error handler middleware

--- apps/server/src/middleware/error-handler-middleware.ts
+++ apps/server/src/middleware/error-handler-middleware.ts
@@ -1,13 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../errors';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ErrorHandlerMiddleware = (err: unknown, req: Request, res: Response, next: NextFunction): void => {
-  console.error(err);
-  res.status(500).send({
-    message: 'Internal Server Error',
-    data: null,
-    code: 500
-  });
+  console.error(err); // 记录错误日志
+
+  if (err instanceof AppError) {
+    res.status(err.code).json({
+      message: err.message,
+      data: err.data || null,
+      code: err.code
+    });
+  } else {
+    res.status(500).json({
+      message: 'Internal Server Error',
+      data: null,
+      code: 500
+    });
+  }
 };
 
 export default ErrorHandlerMiddleware;
