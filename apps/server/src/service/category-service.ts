89b90e13ae13b22e6bf125e4d7e82d6b1a02b9d2
dribble-njr <wzw15292257101@163.com>
2024-09-29 18:55:12 +0800

    refactor(server): category & commodity

--- apps/server/src/service/category-service.ts
+++ apps/server/src/service/category-service.ts
@@ -1,63 +1,45 @@
-import { BaseService } from './base-service';
 import { Category } from '@imperial-kitchen/types';
+import prisma from '../dao';
 
-export default class CategoryService extends BaseService {
-  constructor() {
-    super();
-  }
+export default class CategoryService {
+  constructor() {}
 
   async getAllCategories() {
-    const res = await this.db.execute(`
-      SELECT 
-        c.id AS category_id, 
-        c.name AS category_name, 
-        c.description AS category_description, 
-        g.id AS good_id, 
-        g.name AS good_name, 
-        g.price AS good_price, 
-        g.description AS good_description
-      FROM 
-        category c
-      LEFT JOIN 
-        commodity g 
-      ON 
-        c.id = g.category_id
-    `);
+    const res = await prisma.category.findMany({});
+    console.log(res);
 
     const categoryMap = new Map<number, Category>();
 
-    res.rows.forEach((row) => {
-      const categoryId = Number(row.category_id);
-      if (!categoryMap.has(categoryId)) {
-        categoryMap.set(categoryId, {
-          id: categoryId,
-          name: String(row.category_name),
-          description: String(row.category_description),
-          goods: []
-        });
-      }
+    // res.rows.forEach((row) => {
+    //   const categoryId = Number(row.category_id);
+    //   if (!categoryMap.has(categoryId)) {
+    //     categoryMap.set(categoryId, {
+    //       id: categoryId,
+    //       name: String(row.category_name),
+    //       description: String(row.category_description),
+    //       goods: []
+    //     });
+    //   }
 
-      if (row.good_id) {
-        categoryMap.get(categoryId)?.goods?.push({
-          id: Number(row.good_id),
-          name: String(row.good_name),
-          price: Number(row.good_price),
-          categoryId: categoryId,
-          description: row.good_description ? String(row.good_description) : undefined
-        });
-      }
-    });
+    //   if (row.good_id) {
+    //     categoryMap.get(categoryId)?.goods?.push({
+    //       id: Number(row.good_id),
+    //       name: String(row.good_name),
+    //       price: Number(row.good_price),
+    //       categoryId: categoryId,
+    //       description: row.good_description ? String(row.good_description) : undefined
+    //     });
+    //   }
+    // });
 
     return Array.from(categoryMap.values());
   }
 
   async createCategory(name: string) {
-    const res = await this.db.execute({ sql: 'INSERT INTO category (name) VALUES (?)', args: [name] });
-    return res.lastInsertRowid;
+    console.log(name, 'createCategory');
   }
 
   async deleteCategory(id: number) {
-    const res = await this.db.execute({ sql: 'DELETE FROM category WHERE id = ?', args: [id] });
-    return !!res.rowsAffected;
+    console.log(id, 'deleteCategory');
   }
 }
--- apps/server/src/service/commodity-service.ts
+++ apps/server/src/service/commodity-service.ts
@@ -1,32 +1,17 @@
 import { Commodity } from '@imperial-kitchen/types';
-import { BaseService } from './base-service';
 
-export default class CommodityService extends BaseService {
-  constructor() {
-    super();
-  }
+export default class CommodityService {
+  constructor() {}
 
   async createCommodity(commodity: Commodity) {
-    const { name, price, description, categoryId } = commodity;
-    const res = await this.db.execute({
-      sql: 'INSERT INTO commodity (category_id, name, price, description) VALUES (?, ?, ?, ?)',
-      args: [categoryId ?? null, name ?? null, price ?? null, description ?? null]
-    });
-    return res.lastInsertRowid;
+    console.log(commodity, 'createCommodity');
   }
 
   async updateCommodity(commodity: Commodity) {
-    const { id, name, price, description, categoryId } = commodity;
-    await this.db.execute({
-      sql: 'UPDATE commodity SET name = ?, price = ?, description = ?, category_id = ? WHERE id = ?',
-      args: [name ?? null, price ?? null, description ?? null, categoryId ?? null, id ?? null]
-    });
-    return commodity;
+    console.log(commodity, 'updateCommodity');
   }
 
   async deleteCommodity(id: number) {
-    const res = await this.db.execute({ sql: 'DELETE FROM commodity WHERE id = ?', args: [id] });
-    console.log(res, 'delete res');
-    return !!res.rowsAffected;
+    console.log(id, 'deleteCommodity');
   }
 }
