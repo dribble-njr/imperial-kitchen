a6eb49d3d9ba8f2167e123050b085385ef751e1a
dribble-njr <wzw15292257101@163.com>
2024-11-04 14:54:46 +0800

    chore: load bundle yaml

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process';
+import { renameSync } from 'node:fs';
+
+const execAsync = promisify(exec);
 
 @Module({
   imports: [SharedModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit() {
+    try {
+      await execAsync('yarn bundle:openapi');
+      renameSync('./openapi/bundled.yaml', './dist/openapi/bundled.yaml');
+      console.log('[OpenAPI] schema bundled successfully');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
