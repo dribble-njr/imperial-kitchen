8f41059bdc701aba118dfe9999fabf79b3fb03e6
dribble-njr <wzw15292257101@163.com>
2024-11-06 19:52:36 +0800

    feat: refresh token

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { RegisterAdminDto, RegisterMemberDto } from './dto/register-user.dto';
 import { TransformResponseInterceptor } from 'src/common/interceptors/transform-response.interceptor';
 import { CaptchaDto } from './dto/captcha.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 @UseInterceptors(TransformResponseInterceptor)
@@ -15,18 +16,22 @@ export class UserController {
     return user;
   }
 
+  @Public()
   @Post('register/admin')
   async registerAdmin(@Body() body: RegisterAdminDto) {
+    console.log(body, 'body');
     const data = await this.userService.registerUser(body, true);
     return data;
   }
 
+  @Public()
   @Post('register/member')
   async registerMember(@Body() body: RegisterMemberDto) {
     const data = await this.userService.registerUser(body, false);
     return data;
   }
 
+  @Public()
   @Get('register/captcha')
   async captcha(@Query() query: CaptchaDto) {
     const data = await this.userService.captcha(query.email);
