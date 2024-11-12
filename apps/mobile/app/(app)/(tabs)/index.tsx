45a1ad4284940d3129030768a63bd07268b00189
dribble-njr <wzw15292257101@163.com>
2024-11-12 17:43:49 +0800

    chore: remove useless content

--- apps/mobile/app/(app)/(tabs)/index.tsx
+++ apps/mobile/app/(app)/(tabs)/index.tsx
@@ -1,50 +1,16 @@
 import { Image, StyleSheet } from 'react-native';
-import { useEffect, useState } from 'react';
-import { Category } from '@imperial-kitchen/types';
 
-import { CategoryList, FoodList, Header } from '@/components/home';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedView } from '@/components/ThemedView';
-import { CategoryService } from '@/service';
-import { ThemedText } from '@/components/ThemedText';
 import { CartProvider } from '@/context/CartContext';
 
 export default function HomeScreen() {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [activeCategoryId, setActiveCategoryId] = useState(0);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const getCategories = async () => {
-    const categories = await CategoryService.getList();
-    setActiveCategoryId(categories[0].id!);
-    setCategories(categories);
-  };
-
-  useEffect(() => {
-    // getCategories();
-  }, []);
-
   return (
     <CartProvider>
       <ParallaxScrollView
         headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
         headerImage={<Image source={require('@/assets/images/partial-react-logo.png')} style={styles.reactLogo} />}
       >
-        <Header />
-
-        {categories.length ? (
-          <ThemedView style={styles.container}>
-            <ThemedView style={styles.sidebar}>
-              <CategoryList categories={categories} activeCategoryId={activeCategoryId} onCategoryPress={() => {}} />
-            </ThemedView>
-
-            <ThemedView style={styles.main}>
-              <FoodList categories={categories} onScroll={() => {}} />
-            </ThemedView>
-          </ThemedView>
-        ) : (
-          <ThemedText>loading...</ThemedText>
-        )}
+        HomeScreen
       </ParallaxScrollView>
     </CartProvider>
   );
