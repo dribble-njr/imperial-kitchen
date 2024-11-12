e0ecec74e4d512ea16b1601afd33e723143459dd
dribble-njr <wzw15292257101@163.com>
2024-11-12 22:11:13 +0800

    refactor: optimize i18n structure

---
+++ apps/mobile/locales/zh.ts
@@ -0,0 +1,24 @@
+const Chinese = {
+  welcome: '欢迎来到御膳房',
+  skip: '跳过',
+  startTip: '登录或注册开始使用',
+  slogan: '定制您的厨房菜单',
+  createKitchen: '创建厨房',
+  joinKitchen: '加入厨房',
+
+  alreadyHaveAccount: '已有账号？',
+  signIn: '登录',
+  logout: '登出',
+  signUp: '注册',
+  enterEmail: '输入邮箱',
+  enterPhone: '输入手机号',
+  enterName: '输入用户名',
+  enterPassword: '输入密码',
+  enterPasswordAgain: '再次输入密码',
+  enterInviteCode: '输入邀请码',
+  forgotPassword: '忘记密码？',
+  sendVerificationCode: '发送验证码',
+  greeting: '你好, {{name}}!'
+};
+
+export default Chinese;
