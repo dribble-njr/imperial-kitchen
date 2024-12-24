16250aa55ab8ddac80df3a671c5d93622ee91ca8
dribble-njr <wzw15292257101@163.com>
2024-12-24 12:43:46 +0800

    feat: use transaction

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -4,29 +4,32 @@ import { PrismaClient, Role } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const kitchen = await prisma.kitchen.create({
-    data: {
-      name: 'Admin Kitchen',
-      inviteCode: generateRandomCode()
-    }
-  });
-  const admin = await prisma.user.upsert({
-    where: { email: 'admin@test.com' },
-    update: {},
-    create: {
-      email: 'admin@test.com',
-      name: 'Admin',
-      password: await hashPassword('admin123'),
-      kitchens: {
-        create: {
-          role: Role.ADMIN,
-          kitchenId: kitchen.id
+  await prisma.$transaction(async (tx) => {
+    const kitchen = await tx.kitchen.create({
+      data: {
+        name: 'Admin Kitchen',
+        inviteCode: generateRandomCode()
+      }
+    });
+
+    const admin = await tx.user.upsert({
+      where: { email: 'admin@test.com' },
+      update: {},
+      create: {
+        email: 'admin@test.com',
+        name: 'Admin',
+        password: await hashPassword('admin123'),
+        kitchens: {
+          create: {
+            role: Role.ADMIN,
+            kitchenId: kitchen.id
+          }
         }
       }
-    }
-  });
+    });
 
-  console.log({ admin });
+    console.log({ admin });
+  });
 }
 main()
   .then(async () => {
